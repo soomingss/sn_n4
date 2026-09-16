7c28456e609e2233bc6968b4146d9b938e9d354d
soomingss <tyuifghj.sh@gmail.com>
2026-09-17 08:36:35 +0900

    Add shared mobile admin back control

---
+++ app/admin/AdminMobileBack.js
@@ -0,0 +1,5 @@
+import Link from "next/link";
+
+export default function AdminMobileBack() {
+  return <div className="adminMobileBackRow contentWidth"><Link href="/admin">관리자 업무로 돌아가기 →</Link></div>;
+}
